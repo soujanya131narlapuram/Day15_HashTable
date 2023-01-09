9249ea3a91e8bf094fd9a2d1545ffb3fb86da1e1
Soujanya <soujanya131narlapuram@gmail.com>
2023-01-09 23:21:35 +0530

    'UC1 Push'

---
+++ src/com/bridgelabz/HashTableMain.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+
+public class HashTableMain {
+
+    public static void main(String[] args) {
+
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] paraArray = paragraph.split(" ");
+        HashTable<String,Integer> hashTable = new HashTable<>();
+        for (String word:paraArray) {
+            Integer currentFrequency = hashTable.get(word);
+            if(currentFrequency == null){
+                hashTable.add(word,1);
+            }else{
+                currentFrequency+=1;
+                hashTable.add(word,currentFrequency);
+            }
+
+        }
+        System.out.println(hashTable);
+    }
+}
